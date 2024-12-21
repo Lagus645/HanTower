b6da16b3291579a2b257605a473805c2b345a141
Lagus645 <lagusu164@gmail.com>
2024-12-21 18:44:14 +0500

    Add files via upload

---
+++ Han/Han/Han.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <clocale>
+using namespace std;
+
+void Han(int n, int start, int point, int temp) {
+	if (n != 0) {
+		Han(n - 1, start, temp, point);
+		cout << start << "=>" << point << endl;
+		Han(n - 1, temp, point, start);
+	}
+}
+
+int  main() {
+	setlocale(LC_ALL, "russian");
+	int n;
+	cout << "Введите количество дисков: ";
+	cin >> n;
+	Han(n, 1, 3, 2);
+	return 0;
+}
